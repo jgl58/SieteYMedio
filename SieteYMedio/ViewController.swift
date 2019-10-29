818a196e3c571c8e2ca4ca2b15e80edaf0344871
Jonay Gilabert López <jgl58@alu.ua.es>
2019-10-29 18:57:24 +0100

    Cargadas imágenes de cartas y manejada logica de los botones para que no se activen cuando se necesiten

--- SieteYMedio/ViewController.swift
+++ SieteYMedio/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     var juego : Juego!
+    var vistasCartas = [UIImageView]()
+    var contador : Int = 1
+    
+    @IBOutlet weak var btnPedir: UIButton!
+    @IBOutlet weak var btnPlantarse: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,20 +24,15 @@ class ViewController: UIViewController {
 
     @IBAction func iniciarJuego(_ sender: Any) {
         juego = Juego()
+        limpiarCartas()
         print("Nueva partida")
-
+        btnPedir.isEnabled = true
+        btnPlantarse.isEnabled = true
         let cartaPedida = juego.pedirCarta()
-        
-        let alert = UIAlertController(
-            title: "Has elegido la carta",
-            message: cartaPedida.descripcion() + " Tu suma es de \(juego.sumaJugador)",
-            preferredStyle: UIAlertController.Style.alert)
-        let action = UIAlertAction(
-            title: "OK",
-            style: UIAlertAction.Style.default)
+        dibujarCarta(carta: cartaPedida, enPosicion: contador)
+        contador+=1
         print(cartaPedida.descripcion() + " Tu suma es de \(juego.sumaJugador)")
-        alert.addAction(action)
-        self.present(alert, animated: true, completion: nil)
+        
     }
     
     @IBAction func plantarse(_ sender: Any) {
@@ -51,16 +51,18 @@ class ViewController: UIViewController {
         print("\(titulo) Tu suma es de \(juego.sumaJugador) y la de la maquina \(res)")
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
+        btnPedir.isEnabled = false
+        btnPlantarse.isEnabled = false
     }
     
     @IBAction func pedirCarta(_ sender: Any) {
         let cartaPedida = juego.pedirCarta()
+        dibujarCarta(carta: cartaPedida, enPosicion: contador)
+        
         if !juego.seguirJugando{
-            print("Te has pasado. \(cartaPedida.descripcion()) Tu suma es de \(juego.sumaJugador)")
-        }else{
             let alert = UIAlertController(
-                title: "Has elegido la carta",
-                message: cartaPedida.descripcion() + " Tu suma es de \(juego.sumaJugador)",
+                title: "Te has pasado",
+                message: "Tu suma es de \(juego.sumaJugador)",
                 preferredStyle: UIAlertController.Style.alert)
             let action = UIAlertAction(
                 title: "OK",
@@ -68,7 +70,53 @@ class ViewController: UIViewController {
             print(cartaPedida.descripcion() + " Tu suma es de \(juego.sumaJugador)")
             alert.addAction(action)
             self.present(alert, animated: true, completion: nil)
+            
+            btnPedir.isEnabled = false
+            btnPlantarse.isEnabled = false
+            //limpiarCartas()
+            print("Te has pasado. \(cartaPedida.descripcion()) Tu suma es de \(juego.sumaJugador)")
         }
+        contador+=1
+    }
+    
+    func dibujarCarta(carta: Carta, enPosicion : Int) {
+        let limitesPantalla = UIScreen.main.bounds
+        let anchoPantalla = limitesPantalla.width
+        let altoPantalla = limitesPantalla.height
+
+        let nombreImagen = String(carta.valor)+String(carta.palo.rawValue)
+        //creamos un objeto imagen
+        let imagenCarta = UIImage(named: nombreImagen)
+        //para que la imagen sea un componente más del UI,
+        //la encapsulamos en un UIImageView
+        let cartaView = UIImageView(image: imagenCarta)
+        //Inicialmente la colocamos fuera de la pantalla y más grande
+        //para que parezca más cercana
+        //"frame" son los límites de la vista, definen pos y tamaño
+        cartaView.frame = CGRect(x: -200, y: -200, width: 200, height: 300)
+        //La rotamos, para que al "repartirla" haga un efecto de giro
+        cartaView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi));
+        //La añadimos a la vista principal, si no no sería visible
+        self.view.addSubview(cartaView)
+        //guardamos la vista en el array, para luego poder eliminarla
+        self.vistasCartas.append(cartaView)
+        //Animación de repartir carta
+        UIView.animate(withDuration: 0.5){
+            //"efecto caida": la llevamos a la posición final
+            cartaView.frame = CGRect(x:50+70*(enPosicion-1), y:100, width:Int(anchoPantalla/4), height:Int(altoPantalla/5));
+            //0 como ángulo "destino", para que rote mientras "cae"
+            cartaView.transform = CGAffineTransform(rotationAngle:0);
+        }
+    }
+    
+    func limpiarCartas(){
+        self.contador = 1
+        //Quitamos las cartas de la pantalla
+        for vistaCarta in self.vistasCartas {
+            vistaCarta.removeFromSuperview()
+        }
+        //ya no tenemos imágenes de cartas en pantalla, ponemos el array a vacío
+        self.vistasCartas=[]
     }
 }
 
