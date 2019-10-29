f9e63f8d838ac263ef6d52c4b5b977d98ca9fc52
Jonay Gilabert López <jgl58@alu.ua.es>
2019-10-29 16:42:52 +0100

    Initial Commit

---
+++ SieteYMedio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SieteYMedio
+//
+//  Created by Máster Móviles on 29/10/2019.
+//  Copyright © 2019 Máster Móviles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
